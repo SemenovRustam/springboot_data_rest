2979197d3742ac5b6c8217e4ab326fc7a4c9a0c0
Rustam Semenov <88612028+SemenovRustam@users.noreply.github.com>
2021-10-27 10:27:34 +0500

    Add files via upload

---
+++ spring_data_rest/src/main/java/com/rustamsemenov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.rustamsemenov.spring.springboot.spring_data_rest.dao;
+
+
+import com.rustamsemenov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
